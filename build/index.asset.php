ab7f62532b3c1106158519b5d88ed52a9c672fb2
fgiannar <fgiannar@users.noreply.github.com>
2021-07-21 12:43:07 +0000

    RNA components: Add Disconnect Dialog (#20402)
    * RNA components: Add Disconnect Dialog
    
    * Add changelog entries.
    
    * DisconnectDialog: Styling adjustments
    
    * DisconnectDialog: Add newlines
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1052513044

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '67ddeae2484e9a39322a529c9b83dbd3');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '892b9a359fdf8137569cfffaea3e097c');
