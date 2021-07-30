7432fc64baa9c1f0e2757eb2eec61dfdcc87d7b6
kraftbj <kraftbj@users.noreply.github.com>
2021-07-30 23:24:45 +0000

    Tools: Update eslint-plugin-jsdoc (#20515)
    * JSDoc updates
    
    * Bump to 35.x
    
    * Bump to 36.x
    
    * Bump beta plugin
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1083845381

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '09f4ce30640e839ffdf7d42e950ba07c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'fca4aeef689c34e37045fdad3165da26');
