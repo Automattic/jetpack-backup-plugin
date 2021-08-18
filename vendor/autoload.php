3aaf45412176277539ca9052a0726fad2081bd41
vianasw <vianasw@users.noreply.github.com>
2021-08-18 12:52:43 +0000

    Update Backup plugin version numbers to 0.2.0
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1143146588

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit9559eef123208b7d1b9c15b978567267_backupⓥ0_2_0_beta::getLoader();
+return ComposerAutoloaderInit9559eef123208b7d1b9c15b978567267_backupⓥ0_2_0::getLoader();
