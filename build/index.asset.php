18f6d57da7c8e3f000bd20bd4ab7e4a829333dc9
jeherve <jeherve@users.noreply.github.com>
2021-09-16 15:27:31 +0000

    Docker: update references to extra config files. (#21111)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1242146398

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '2849bcaa796df253f04404f502ba5df7');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '495227b38394c5993a68f0b7739364fc');
