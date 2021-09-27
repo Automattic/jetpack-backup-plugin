e8f1f3308083e2441e06e5a93b2b048b41eeaa19
mdbitz <mdbitz@users.noreply.github.com>
2021-09-27 18:54:37 +0000

    Jetpack Sync: Checksums: Add support for User and User Meta tables. (#21139)
    * Initial commit
    
    * Change method visibility to allow easier extension on WPCOM.
    
    * Add changelog entry
    
    * Bump version
    
    * Major rewrite to rely on `WP_User` objects. Changed the method visibilities to allow easier extending.
    
    * Add `users` and `usermeta` to `checksum_all`
    
    * Small refactoring to allow better WPCOM integration.
    
    * reorder usermeta checksum to match the umeta_id used on WP.com. update the meta values to use the expand_user logic from the User modules (used during sync) and also apply sanitzation used on WP.com. This will allow for us to checksum directly against the db value.
    
    * Count the items distinctively. This is temporary and should be fixed in a more general sense.
    
    * Update the checksum generation to align with WP.com logic summation of individual key-values
    
    * not applicable comment
    
    * Distinct Usermeta count query
    empty check for meta.
    
    * Update the usermeta mapping to reference the users table directly instead of usermeta
    
    Co-authored-by: Matthew Denton <matt@mdbitz.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1279551360

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -107,6 +107,7 @@ class ComposerStaticInit9559eef123208b7d1b9c15b978567267_backupⓥ0_3_0_alpha
         'Automattic\\Jetpack\\Sync\\REST_Sender' => __DIR__ . '/..' . '/automattic/jetpack-sync/src/class-rest-sender.php',
         'Automattic\\Jetpack\\Sync\\Replicastore' => __DIR__ . '/..' . '/automattic/jetpack-sync/src/class-replicastore.php',
         'Automattic\\Jetpack\\Sync\\Replicastore\\Table_Checksum' => __DIR__ . '/..' . '/automattic/jetpack-sync/src/replicastore/class-table-checksum.php',
+        'Automattic\\Jetpack\\Sync\\Replicastore\\Table_Checksum_Usermeta' => __DIR__ . '/..' . '/automattic/jetpack-sync/src/replicastore/class-table-checksum-usermeta.php',
         'Automattic\\Jetpack\\Sync\\Replicastore_Interface' => __DIR__ . '/..' . '/automattic/jetpack-sync/src/interface-replicastore.php',
         'Automattic\\Jetpack\\Sync\\Sender' => __DIR__ . '/..' . '/automattic/jetpack-sync/src/class-sender.php',
         'Automattic\\Jetpack\\Sync\\Server' => __DIR__ . '/..' . '/automattic/jetpack-sync/src/class-server.php',
