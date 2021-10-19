320329a97f4894193a4f0e0d5037da6856d3fcee
leogermani <leogermani@users.noreply.github.com>
2021-10-19 20:44:53 +0000

    Update/connect button visual component (#21389)
    * Create ConnectButtonVisual component
    
    * rename folder and add it to loaders paths
    
    * expose component to generate docs
    
    * use the new visual component
    
    * use controls instead of knobs and better docs
    
    * add actions - not working yet
    
    * simplify visual components props
    
    * rename and move to components/action button
    
    * Fix imports and button label
    
    * add changelog
    
    * Fix typo
    
    * update pnpm lock
    
    * bump packages versions
    
    * update dependencies
    
    * please linter
    
    * Move tests to the visual component
    
    * Fix internal dependencies
    
    * changelogs
    
    * bump backup version
    
    * update dependencies
    
    * Actionbutton label prop is required
    
    * typo in comment
    
    * fix typo in variable name
    
    * rename css classes
    
    * updating pnpm lock
    
    * updating internal dependencies
    
    * updating internal dependencies
    
    * update internal dependencies
    
    * updating internal dependencies
    
    * update references to action button css class
    
    Co-authored-by: Sergey Mitroshin <sergeymitr@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1360844848

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '680cc9958b6036f7cbacbcb7dc789f32');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '5fb53a2bd0138163768a1ad2dcd1a5d3');
