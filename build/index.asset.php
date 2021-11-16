e360492616f0e44c136ec99e73da3e2141fa15c1
jeherve <jeherve@users.noreply.github.com>
2021-11-16 15:19:20 +0000

    Button styles: ensure connection button can accomodate longer text (#21711)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1467438839

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7567877ea4df748e3cbb4bbf830d4cb9');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '47d458c2f0b5e2587bf9702f9b8a59ef');
