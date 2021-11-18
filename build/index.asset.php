6a085b599e011253babc720b2a846c96e5bcff6b
anomiex <anomiex@users.noreply.github.com>
2021-11-18 22:37:40 +0000

    Update dependency css-loader to v6.5.1 (#21812)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: matticbot <matticbot@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1478615094

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4f33ae665165404a2d85ef3e505a3bd2');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd947b821c1506801b40d936f09bf66eb');
