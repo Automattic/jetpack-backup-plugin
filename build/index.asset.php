63c4cdb1d812f25299d10d612517fd6e4a4e55b0
oskosk <oskosk@users.noreply.github.com>
2022-01-25 18:58:26 +0000

    Connection: Fix access to display_name property in connection status card (#22486)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1747132418

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '2bd501e8264bf23869fae2c4cc8ea555');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '976886d79d9cf89d7127afaf23b1ac32');
