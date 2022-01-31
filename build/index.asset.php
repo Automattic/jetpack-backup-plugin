990ff6e6f973f64da008456a321853dc04ffef31
samiff <samiff@users.noreply.github.com>
2022-01-31 20:24:18 +0000

    Track user clicks on Links/Buttons in disconnect modal footer (#22467)
    * [not verified] Track user clicks on disconnect dialog modal
    
    * Fix and add some unit tests
    
    * Changelog
    
    * changes based on review comments
    
    - rename stay connected handler
    - move all click tracking code to step-disconnect
    
    * Update based on comments
    
    - fix tests to loop and be less brittle
    - stop constructing event names programmatically
    - some formatting
    
    * version fixup script
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1774678443

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '0ed057029bd9c9d5e87700ae8530e003');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'fa2aeeef556792decbee4d7b5f2cbd4a');
