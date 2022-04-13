598075737b7ec5baf11d060acadc831716b3a8c1
kangzj <kangzj@users.noreply.github.com>
2022-04-13 00:23:08 +0000

    Search: Add search product tier pricing (#23903)
    * [not verified] added rest API and client support for search pricing
    
    * bump api versions
    
    Co-authored-by: Jason Moon <jason.moon@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2158242759

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '62c0ed1f7dd9fc64bae94b468cfa55f7');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '774760de370da03fbc2d26d3b8e42f47');
