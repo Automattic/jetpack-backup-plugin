d4095187b3f71f4529cca2a9025be83987a25909
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-04-14 02:03:18 +0000

    Add TypeScript support (#23522)
    * [not verified] Add tsconfig to root and jetpack plugin
    
    * [not verified] Move RecordMeterBar to js-components and convert to typescript
    
    * [not verified] Convert BackupUpgrade and BarChart to typescript
    
    * [not verified] Add stories and unit test for RecordMeterBar
    
    * [not verified] Add tsconfig to js-packages
    
    * [not verified] Add types and dependencies
    
    * [not verified] Fix storybook config
    
    * [not verified] Fix unit test configuration
    
    * [not verified] Remove TS config for ESLint for now to avoid too many lints
    
    * [not verified] Exclude tsconfig files from production
    
    * [not verified] Remove enzyme and chai and use their types instead
    
    * [not verified] Move react rule to eslint/react.js
    
    * [not verified] Revert changes to RecordMeterbar and BackupUpgrade components
    
    This reverts commit d25281b77ca77865308f5ccb3f4a74d5eccda496.
    
    [not verified] Revert "[not verified] Properly fix changelog files"
    
    This reverts commit 4b879db3388da44336fb95343ea81eec1748c2c3.
    
    [not verified] Revert "[not verified] Fix changelog"
    
    This reverts commit 443cdbed2c724f7848d82c32a6d5e5b404091a0f.
    
    [not verified] Revert "[not verified] Update -fix-add-ts-vs-code-support"
    
    This reverts commit 495fe38b864b819930242fac16d7f94d056539f7.
    
    [not verified] Revert "[not verified] Fix changelog"
    
    This reverts commit 6cbcbb9e2cb6de0080fdefe3be347040663fbeec.
    
    [not verified] Revert "[not verified] Fix changelog file"
    
    This reverts commit a085db8bd4f982198ff4debc6086ba28e1c7a429.
    
    [not verified] Revert "[not verified] Add changelog files"
    
    This reverts commit 094bf512dd89dee18336ebd5f8cc63f4b827b32a.
    
    [not verified] Revert "[not verified] Add stories and unit test for RecordMeterBar"
    
    This reverts commit e6173d8bad64924723a679f9b35caa0757fb6694.
    
    Revert "[not verified] Convert BackupUpgrade and BarChart to typescript"
    
    This reverts commit b2a5bc76b6395d5abc377554fdb6bca93543a4a7.
    
    * [not verified] Remove settings.jsdoc.mode after using @typescript-eslint/parser
    
    * [not verified] Fix webpack resolution for internal js-packages by using resolve.alias
    
    * [not verified] Update changelogs
    
    * [not verified] Revert "[not verified] Fix webpack resolution for internal js-packages by using resolve.alias"
    
    This reverts commit 1b22a591cd102f17f2770c2f46073885717e543c.
    
    * [not verified] Remove irrelevant changelogs
    
    * [not verified] Clean up
    
    * [not verified] Re-create root ts config
    
    * [not verified] Update tsconfig for jetpack and js-packages
    
    * [not verified] Clean up root tsconfig
    
    * [not verified] Move tsconfig to js-tools
    
    * [not verified] Update pnpm-lock.yaml
    
    * Update pnpm-lock.yaml
    
    * Fix project versions
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2164719228

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '774760de370da03fbc2d26d3b8e42f47');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '0edf8665f3acb8b2e7338cdb81f3407f');
