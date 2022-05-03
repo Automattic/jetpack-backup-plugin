d8b5b7c3901e3bace43b36a86c239c8bb8fdd3c7
jeherve <jeherve@users.noreply.github.com>
2022-05-03 17:32:48 +0000

    General: remove backwards compatibility code now that Jetpack requires WP 5.9 (#24086)
    * General: remove backwards compatibility code for user queries
    
    See #22559
    
    * Update PHPUnit setup to remove WP < 5.9 oddities
    
    See #21175, #21157
    
    Primary issue: #24082
    
    * Update docs/examples/bootstrap.php
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    * Remove speed trap listener loder
    
    * Update doc to remove mention of temporary PHPUnit situation
    
    * Remove temporary line break workaround
    
    See #15595
    
    * Remove custom function in favor of the one that ships with core
    
    * Remove old workaround for the Jetpack sidebar plugin icon
    
    See #14327
    
    * Remove PHP 5.2 workaround
    
    See 99fffd8f5425f2d77d9d621acddd7561d621ac39
    
    * Add back necessary _wp_specialchars wrapping
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2265284528

---
+++ jetpack_vendor/automattic/jetpack-identity-crisis/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'd884cbb3252c0010e135139ee2e4b194');
