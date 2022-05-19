f512e1646808c807b16502f29499b55c8520f044
jeherve <jeherve@users.noreply.github.com>
2022-05-19 17:36:33 +0000

    Jetpack: update changelog with 10.9.1 entries (#24435)
    * Update changelog and readme for point release
    
    * Update stable version to current stable
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2353566122

--- jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'c1b992498b885833d82dce1ba239ff1a');
