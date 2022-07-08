d99792d29586efc834550c4344756a4b9472ef11
renatoagds <renatoagds@users.noreply.github.com>
2022-07-08 13:03:59 +0000

    VideoPress: Improve layout for upload error message (#24963)
    * VideoPress: Improve upload error message ux
    
    * [not verified] VideoPress: Add changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2636182350

--- jetpack_vendor/automattic/jetpack-backup/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '09030e8f2a5e758f12eb');
--- jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a7d62da3248ac91ce129');
