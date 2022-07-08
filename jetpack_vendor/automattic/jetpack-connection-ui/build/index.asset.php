f570292f9fee848513f2ba1179016f2c1c6d265a
elliottprogrammer <elliottprogrammer@users.noreply.github.com>
2022-07-08 13:25:29 +0000

    Make Backup plugin more clear about realtime backups. (#24955)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2636295539

---
+++ jetpack_vendor/automattic/jetpack-connection-ui/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b8efa14e096c837c8702');
---
+++ jetpack_vendor/automattic/jetpack-identity-crisis/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '8730c73a004d09ae27c0');
