d5ed60325f07dda7b2dd204d72520d875fb312c7
zinigor <zinigor@users.noreply.github.com>
2023-03-28 21:59:55 +0000

    Merge a prerelease branch that was erroneously removed. (#29762)
    * Jetpack 12.0-beta.
    
    * Jetpack Social 1.9.0-beta.
    
    * Jetpack Backup 1.6.0-beta.
    
    * Jetpack mu-wpcom-plugin 1.0.12.
    
    * Squashed changelog.
    
    * Restored Jetpack changes.
    
    * Reverted Social.
    
    * Reverted Backup.
    
    * Reverting version changes.
    
    * Removed unintended change.
    
    * Reverted Backup.
    
    * Reverted mu-plugin-wpcom.
    
    * Reverted unnecessary changes to Social.
    
    * Removed unneeded changelogs.
    
    * Fixed up project versions.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4547912867

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -14,15 +14,15 @@ return array(
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '1.12.8-alpha',
+      'ver' => '1.12.8',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '1.15.0-alpha',
+      'ver' => '1.15.0',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.51.3-alpha',
+      'ver' => '1.51.3',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '2.3.3-alpha',
+      'ver' => '2.3.4-alpha',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '2.9.0-alpha',
+      'ver' => '2.9.1-alpha',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
