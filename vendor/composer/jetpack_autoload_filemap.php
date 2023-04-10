4aeee53b865d73b8c19afe821e18e55d8e64c780
jeherve <jeherve@users.noreply.github.com>
2023-04-10 10:10:03 +0000

    Packages: add Autoloader suggestion (#29988)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4656408419

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,11 +7,11 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '1.18.0.0',
+		'version' => '1.18.1.0-alpha',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'd4eb94df91a729802d18373ee8cdc79f' => array(
-		'version' => '1.12.11.0',
+		'version' => '1.12.12.0-alpha',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-backup/actions.php'
 	),
 );
