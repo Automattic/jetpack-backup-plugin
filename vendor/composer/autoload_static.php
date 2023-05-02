06617f95e16fa44f3224cf5a2514c3c65d5d6992
zinigor <zinigor@users.noreply.github.com>
2023-05-02 15:48:14 +0000

    Releasing backup 1.7.
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4862962191

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit9559eef123208b7d1b9c15b978567267_backupⓥ1_7_beta
+class ComposerStaticInit9559eef123208b7d1b9c15b978567267_backupⓥ1_7
 {
     public static $files = array (
         '3773ef3f09c37da5478d578e32b03a4b' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-assets/actions.php',
@@ -186,9 +186,9 @@ class ComposerStaticInit9559eef123208b7d1b9c15b978567267_backupⓥ1_7_beta
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit9559eef123208b7d1b9c15b978567267_backupⓥ1_7_beta::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit9559eef123208b7d1b9c15b978567267_backupⓥ1_7_beta::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit9559eef123208b7d1b9c15b978567267_backupⓥ1_7_beta::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit9559eef123208b7d1b9c15b978567267_backupⓥ1_7::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit9559eef123208b7d1b9c15b978567267_backupⓥ1_7::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit9559eef123208b7d1b9c15b978567267_backupⓥ1_7::$classMap;
 
         }, null, ClassLoader::class);
     }
