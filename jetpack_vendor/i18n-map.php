711bb3119db4e0ad6f80085ce54e1b7d708ba6be
zinigor <zinigor@users.noreply.github.com>
2023-08-01 19:40:39 +0000

    Backport Jetpack 12.5-a.1 Changes (#32199)
    * Changelog edits.
    
    * Start new Jetpack Cycle
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5730796582

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -14,7 +14,7 @@ return array(
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '1.16.5-alpha1690575738',
+      'ver' => '1.16.5',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -22,11 +22,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '1.56.0-alpha1690378962',
+      'ver' => '1.56.0',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.9.0-alpha1690378962',
+      'ver' => '0.9.0',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '3.2.1-alpha1690903910',
+      'ver' => '3.2.1',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -54,7 +54,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '1.52.0-alpha1690799800',
+      'ver' => '1.52.0',
     ),
   ),
 );
