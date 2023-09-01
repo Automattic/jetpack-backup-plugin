1a5bc1893336475bd3e0f28541bfee5943f8a4f5
anomiex <anomiex@users.noreply.github.com>
2023-09-01 18:05:56 +0000

    Update wordpress monorepo (#32803)
    * Update wordpress monorepo
    * Skip `@wordpress/eslint-plugin` for now, will do that separately to
      more clearly coordinate updating other deps.
    * Cleanup StaticSiteGeneratorPlugin globals in webpack.config.extensions.js
      Having `navigator: {}` was breaking framer-motion, and it turns out
      that and most of the rest are no longer needed.
    * Work around typo in y-webrtc and y-protocols package.json
      They had `peerDependenies` rather than `peerDependencies`. Aready
      fixed upstream, but unreleased.
    * Clean up no-longer-needed pnpmfile overrides
    * Work around missing `@babel/runtime` dep in `@wordpress/patterns`
    * Update serialization in jetpack mailchimp block test
      Looks like some GB change ([#53521]?) got better at omitting default values
    * Update button test for changed popover container
      Probably [#53889]
    
    [#53521]: https://href.li?https://github.com/wordpress/gutenberg/pull/53521
    [#53889]: https://href.li?https://github.com/wordpress/gutenberg/pull/53889
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6052363633

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,11 +7,11 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '1.18.9.0',
+		'version' => '1.18.10.0-alpha1693589610',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'd4eb94df91a729802d18373ee8cdc79f' => array(
-		'version' => '1.17.3.0',
+		'version' => '1.17.4.0-alpha1693589610',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-backup/actions.php'
 	),
 );
