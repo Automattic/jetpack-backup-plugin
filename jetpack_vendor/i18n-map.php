1c05b368edf39e8d6f296b9c2cd89efc73ae94c2
anomiex <anomiex@users.noreply.github.com>
2023-12-11 20:50:43 +0000

    Backport mu-wpcom-plugin 2.0.5, jetpack 13.0-a.1 Changes (#34585)
    * Add changelogs
    
    * Changelog edits.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7173088707

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.0.3-alpha1701883615',
+      'ver' => '2.0.3',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '2.0.2-alpha1701712806',
+      'ver' => '2.0.2',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.1.3-alpha1702059666',
+      'ver' => '4.1.3',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -54,7 +54,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '2.2.0-alpha1702056376',
+      'ver' => '2.2.0',
     ),
   ),
 );
