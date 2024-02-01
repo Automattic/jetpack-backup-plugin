5bc162269f24e0384a3241e9cb9ef9bef7a5c974
anomiex <anomiex@users.noreply.github.com>
2024-02-01 19:27:14 +0000

    Update wordpress monorepo (#35384)
    * Update wordpress monorepo
    * Apparently GB changed something that eliminated some duplicate class names
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7746415821

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.1.0-alpha1706803348',
+      'ver' => '2.1.0-alpha1706814673',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '3.1.1',
+      'ver' => '3.1.2-alpha1706814673',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
@@ -22,11 +22,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.3.0-alpha1706725881',
+      'ver' => '2.3.0-alpha1706814673',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
-      'ver' => '0.15.1-alpha1706626514',
+      'ver' => '0.15.1-alpha1706814673',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '3.0.2',
+      'ver' => '3.0.3-alpha1706814673',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.7.1-alpha1706562411',
+      'ver' => '4.7.1-alpha1706814673',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
