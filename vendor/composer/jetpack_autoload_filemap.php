09f5a4195ec1a3c7e5bc59aa7b7223557f78c16c
anomiex <anomiex@users.noreply.github.com>
2024-04-08 08:15:50 +0000

    Fix obviously wrong class name references (#36769)
    Almost all are phpdoc comments where the class was missing a `use` or
    leading `\`. More specifically, I searched for cases where Phan was
    complaining about classes named like `\Automattic\...\Automattic\`, or
    `\WP_...`, or `Exception` classes, and fixed all the obvious ones.
    
    Then I also searched for "undeclared {class|type} \[a-z]" and fixed the
    obvious ones out of that too (e.g. "obj", "number", and various typos).
    
    Note that having the correct typing allows Phan to complain about
    possibly-real issues in some cases. Those will have to be reviewed
    later.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8596601938

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -11,7 +11,7 @@ return array(
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'd4eb94df91a729802d18373ee8cdc79f' => array(
-		'version' => '3.3.6.0-alpha1712337314',
+		'version' => '3.3.6.0-alpha1712563319',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-backup/actions.php'
 	),
 );
