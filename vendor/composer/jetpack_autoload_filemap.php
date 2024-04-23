2c69a05ef13dc110fcd6fa0735c06a828119a009
anomiex <anomiex@users.noreply.github.com>
2024-04-23 15:31:56 +0000

    Backport mu-wpcom-plugin 2.1.16, jetpack 13.4-a.5 Changes (#37006)
    Changelog and readme.txt edits.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8802943091
    
    Upstream-Ref: Automattic/jetpack@908087142c492b9e3bfa116faa4c8fca0caef7e0

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,11 +7,11 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '2.1.8.0-alpha1713286100',
+		'version' => '2.1.8.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'd4eb94df91a729802d18373ee8cdc79f' => array(
-		'version' => '3.3.8.0-alpha1713276832',
+		'version' => '3.3.8.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-backup/actions.php'
 	),
 );
