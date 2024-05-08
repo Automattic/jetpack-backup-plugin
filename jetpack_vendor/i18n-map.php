8df40dfb75c202427193283a167dc9ebafa691d7
dilirity <dilirity@users.noreply.github.com>
2024-05-08 10:58:26 +0000

    Backport boost 3.3.0 Changes (#37280)
    * Changelog and readme.txt edits.
    
    * Version bumps
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9000502569
    
    Upstream-Ref: Automattic/jetpack@619b282739a03cd951fce607ca9ab734b928a32a

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -14,7 +14,7 @@ return array(
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '3.3.10',
+      'ver' => '3.3.11-alpha1715165104',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.23.0',
+      'ver' => '4.23.1',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -62,7 +62,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '2.16.0-alpha1715114786',
+      'ver' => '2.16.0',
     ),
   ),
 );
