0689a5026c65fce4f458f4c22347aa784faf8f48
anomiex <anomiex@users.noreply.github.com>
2024-05-17 22:23:09 +0000

    phan: Add "stubs" for in-monorepo plugins (#37442)
    But instead of really creating stub files, just reference the relevant
    files directly from the Phan config. This is easier for what's hopefully
    usually a temporary situation.
    
    There are also a handful of fixes, and in packages/image-cdn some
    suppressions of the callable warnings for a bunch of
    `remove_filter()`/`remove_action()` calls.
    
    P.S. At some point we should probably mark methods like
    `Jetpack::is_module_active()` as `@deprecated` so Phan will (mostly)
    flag them as such for fixing.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9134793823
    
    Upstream-Ref: Automattic/jetpack@a913e80050f1923f6ef024d7fbd69ea86e1bc1d5

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.8.3-alpha1715975237',
+      'ver' => '2.8.3-alpha1715983705',
     ),
     'jetpack-idc' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.23.3-alpha1715799056',
+      'ver' => '4.23.3-alpha1715983705',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -62,7 +62,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '2.16.4-alpha1715957740',
+      'ver' => '2.16.4-alpha1715983705',
     ),
   ),
 );
