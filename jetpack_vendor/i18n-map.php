20f249fe2616ea030ee82414dccfc6282eea679d
anomiex <anomiex@users.noreply.github.com>
2024-06-04 17:00:12 +0000

    Update wordpress monorepo (#37669)
    * Update wordpress monorepo
    * Pnpmfile hack for `@wordpress/primitives` missing `react` dep
    * jest: Mock `client-zip`, it doesn't even try to work with jest.
    * Update tests for wordpress/gutenberg#61366
    * Don't upgrade `@wordpress/dependency-extraction-webpack-plugin` past 5.9.0
    * Add pnpmfile hack to prevent accidentally re-updating `@wordpress/dependency-extraction-webpack-plugin`.
    * Update `@automattic/social-previews` pnpmfile hack to avoid duplicate `@wordpress/*` package versions.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9370984130
    
    Upstream-Ref: Automattic/jetpack@3273e03932bb8d55d574ae540493b536b1bdbf2b

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.1.12-alpha1717449204',
+      'ver' => '2.1.12-alpha1717519483',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '3.3.14',
+      'ver' => '3.3.15-alpha1717519483',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.9.1',
+      'ver' => '2.9.2-alpha1717519483',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '3.1.11',
+      'ver' => '3.1.12-alpha1717519483',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.24.2',
+      'ver' => '4.24.3-alpha1717519483',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,11 +7,11 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '2.1.12.0-alpha1717449204',
+		'version' => '2.1.12.0-alpha1717519483',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'd4eb94df91a729802d18373ee8cdc79f' => array(
-		'version' => '3.3.14.0',
+		'version' => '3.3.15.0-alpha1717519483',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-backup/actions.php'
 	),
 );
