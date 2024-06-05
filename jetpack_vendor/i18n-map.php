9ae6c8cfa9a81b21e0098bda68c9725c007a2b13
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2024-06-05 06:32:19 +0000

    Changelog and readme.txt edits. (#37710)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9379163134
    
    Upstream-Ref: Automattic/jetpack@f4d6546322d889a49170b1a81df9e0d1fd6845cc

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.1.12-alpha1717519483',
+      'ver' => '2.1.12',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '2.9.2-alpha1717527714',
+      'ver' => '2.9.2',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -38,7 +38,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '3.1.12-alpha1717519483',
+      'ver' => '3.1.12',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.24.3-alpha1717519483',
+      'ver' => '4.24.3',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
