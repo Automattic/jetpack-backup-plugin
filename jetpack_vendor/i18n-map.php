1435f49831c07be22b348e0edb7e6c58395c8a48
zinigor <zinigor@users.noreply.github.com>
2024-06-13 14:14:46 +0000

    Backport  Changes (#37847)
    * Changelog and readme.txt edits.
    
    * Version bumps
    
    * Revert "Version bumps"
    
    This reverts commit b30f148f4f98f0d1d0a503420fcf8a45a6b2530e.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9501055677
    
    Upstream-Ref: Automattic/jetpack@9742c21831c9c63896a5161f3272cec41eb50b23

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -58,7 +58,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '3.1.1-alpha1718275163',
+      'ver' => '3.1.1',
     ),
   ),
 );
