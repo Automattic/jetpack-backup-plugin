d7ee22154d65ad16fd6f83fe72d1499756d4a0eb
Initsogar <Initsogar@users.noreply.github.com>
2024-06-27 18:48:35 +0000

    Updated package versions for automattic/jetpack-backup-plugin
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9701972289
    
    Upstream-Ref: Automattic/jetpack@226ad8d54569910e1e8effe7b70b49e3ad95167c

--- vendor/jetpack-autoloader/class-path-processor.php
+++ vendor/jetpack-autoloader/class-path-processor.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp9559eef123208b7d1b9c15b978567267_backupⓥ2_7_alpha\al3_0_8;
+namespace Automattic\Jetpack\Autoloader\jp9559eef123208b7d1b9c15b978567267_backupⓥ2_7\al3_0_8;
 
  // phpcs:ignore
 
