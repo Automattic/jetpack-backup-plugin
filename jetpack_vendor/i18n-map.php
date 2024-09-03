24e9f9dc51fe7773fcebba52424f5899efb76314
anomiex <anomiex@users.noreply.github.com>
2024-09-03 13:58:19 +0000

    Update wordpress monorepo (#39176)
    * Update wordpress monorepo
    * js-packages/components: ToggleControl: Update styles for WordPress/gutenberg#63490
      That change started setting a height on the control itself (versus
      just the stuff inside it), so update our styles to override that too.
    * Add `__nextHasNoMarginBottom` all over the place
      The vast majority are inside a `PanelBody`, which already overrides
      margins. Had to fix one margin on the simple-payments block.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10684158857
    
    Upstream-Ref: Automattic/jetpack@fe2a3d1e65e8e8bd3a4488c719ae2de61b05f026

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.3.5',
+      'ver' => '2.3.6-alpha1725370896',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '3.4.6',
+      'ver' => '3.4.7-alpha1725370896',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '3.0.0-alpha1725370413',
+      'ver' => '3.0.0-alpha1725370896',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.1.5',
+      'ver' => '0.1.6-alpha1725370896',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '3.1.19',
+      'ver' => '3.1.20-alpha1725370896',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '4.34.1-alpha1725370669',
+      'ver' => '4.34.1-alpha1725370896',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
