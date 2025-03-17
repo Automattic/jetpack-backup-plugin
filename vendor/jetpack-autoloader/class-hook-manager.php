e6b5160e7275d013dcf592e727dd1db510d8f691
tbradsha <tbradsha@users.noreply.github.com>
2025-03-17 17:31:00 +0000

    Changelog and readme.txt edits. (#42510)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13905647758
    
    Upstream-Ref: Automattic/jetpack@726868daae17d1ab8f6ab5a7335845cf4851fadd

--- vendor/jetpack-autoloader/class-hook-manager.php
+++ vendor/jetpack-autoloader/class-hook-manager.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp9559eef123208b7d1b9c15b978567267_backupⓥ3_2_alpha\al5_0_4_alpha;
+namespace Automattic\Jetpack\Autoloader\jp9559eef123208b7d1b9c15b978567267_backupⓥ3_2_alpha\al5_0_4;
 
  // phpcs:ignore
 
