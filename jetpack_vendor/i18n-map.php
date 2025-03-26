be667ee2a1f8bd3c6247bb52a1c5c610df846454
anomiex <anomiex@users.noreply.github.com>
2025-03-26 14:38:04 +0000

    phpunit: Remove unnecessary uses of `@before`, `@after`, etc (#42706)
    Back when we still supported PHP <=7.1, we had to add these in a lot of
    places to work around the fact that PHPUnit 8 started declaring a `void`
    return type on `setUp()`, `tearDown()`, and so on, which 7.1 and earler
    don't support.
    
    But we've since dropped support for PHP <7.2, so we can swtich back to
    the normal functions. It's more straightforward to do that than to start
    adding attributes (`#[Before]` and such) for PHPUnit 11+ where the
    annotations are deprecated.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14085626545
    
    Upstream-Ref: Automattic/jetpack@617a716856cc1f098554c281bb51f670814aaecf

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.14',
+      'ver' => '4.0.15-alpha1742998768',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '4.0.19-alpha1742846008',
+      'ver' => '4.0.19-alpha1742998768',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.9.0',
+      'ver' => '6.9.1-alpha1742998768',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -38,11 +38,11 @@ return array(
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.4.6',
+      'ver' => '0.4.7-alpha1742998768',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.8',
+      'ver' => '4.2.9-alpha1742998768',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.9.2',
+      'ver' => '5.9.3-alpha1742998768',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.9.2',
+      'ver' => '4.9.3-alpha1742998768',
     ),
   ),
 );
