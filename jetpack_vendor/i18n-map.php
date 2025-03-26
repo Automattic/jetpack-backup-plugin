ab6699153540f383d67da84a5cb6f3fa4d09b46b
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2025-03-26 13:06:59 +0000

    Changelog and readme.txt edits. (#42717)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14083716464
    
    Upstream-Ref: Automattic/jetpack@85cf2195e4ed5eb9035adecbad1f7a837875487b

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -22,15 +22,15 @@ return array(
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.4.7-alpha1742931839',
+      'ver' => '0.4.7',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
-      'ver' => '3.1.0-alpha1742846885',
+      'ver' => '3.1.0',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.9.0-alpha1742931839',
+      'ver' => '6.9.0',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.8-alpha1742908850',
+      'ver' => '4.2.8',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.9.2-alpha1742940791',
+      'ver' => '5.9.2',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
