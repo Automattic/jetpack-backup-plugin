7f13083cf7c23eb4e0d0ae6c4401795c8ef108de
tbradsha <tbradsha@users.noreply.github.com>
2025-04-14 15:25:02 +0000

    Changelog and readme.txt edits. (#43068)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14449127001
    
    Upstream-Ref: Automattic/jetpack@a5a5f1073c79e984723096183510a993fef3682d

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,15 +7,15 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '4.0.18.0-alpha1744299365',
+		'version' => '4.0.18.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'7372b7fb88a9723cf5b76d456eb0b738' => array(
-		'version' => '6.11.0.0-alpha1744632491',
+		'version' => '6.11.0.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/actions.php'
 	),
 	'd4eb94df91a729802d18373ee8cdc79f' => array(
-		'version' => '4.0.22.0-alpha1744396387',
+		'version' => '4.0.22.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-backup/actions.php'
 	),
 );
