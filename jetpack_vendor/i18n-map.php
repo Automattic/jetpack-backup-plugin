041a4aa72561720b4f22606454d97c8c6202d798
tbradsha <tbradsha@users.noreply.github.com>
2025-05-12 20:08:12 +0000

    Backport jetpack 14.7-a.1 Changes (#43438)
    * Changelog and readme.txt edits.
    
    * Version bumps
    
    * Fix changelog link
    
    * Oops
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14981257606
    
    Upstream-Ref: Automattic/jetpack@c0f392024ace8af17c98260c9860a6af1164f92b

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -14,7 +14,7 @@ return array(
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '4.2.0-alpha1747061946',
+      'ver' => '4.2.0',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.11.3-alpha1747061946',
+      'ver' => '6.11.3',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.2.19-alpha1746709683',
+      'ver' => '0.2.19',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.16-alpha1747061946',
+      'ver' => '4.2.16',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.14.0-alpha1747061946',
+      'ver' => '5.14.0',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
