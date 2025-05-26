0160e4c38350612077af789bc4a71ac8bfe95ac8
tbradsha <tbradsha@users.noreply.github.com>
2025-05-26 14:51:23 +0000

    Backport jetpack 14.7-a.7 Changes (#43617)
    * Changelog and readme.txt edits.
    
    * Fix typo
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    * Improve changelog entry
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    * Clean up entries
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    * Bump to 14.7-a.7
    
    ---------
    
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15256497081
    
    Upstream-Ref: Automattic/jetpack@50c2dd4318183354a550ea903adb30718297b52b

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.22-alpha1747937056',
+      'ver' => '4.0.22',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '4.2.2-alpha1748022220',
+      'ver' => '4.2.2',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.11.7-alpha1748022220',
+      'ver' => '6.11.7',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.2.21-alpha1747937056',
+      'ver' => '0.2.21',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.19-alpha1748022220',
+      'ver' => '4.2.19',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.14.5-alpha1748232230',
+      'ver' => '5.14.5',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
