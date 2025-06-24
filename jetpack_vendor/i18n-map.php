c93f5d8ebbb63011709611a16400d3495cdf50d9
tbradsha <tbradsha@users.noreply.github.com>
2025-06-24 13:30:11 +0000

    Changelog and readme.txt edits. (#44061)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15851444438
    
    Upstream-Ref: Automattic/jetpack@8470dea24b54e954d89598f72ca9c4d1479aaca8

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -14,7 +14,7 @@ return array(
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '4.2.6-alpha1750347518',
+      'ver' => '4.2.6',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.13.8-alpha1750695105',
+      'ver' => '6.13.8',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.16.5',
+      'ver' => '5.16.6',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -11,11 +11,11 @@ return array(
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'7372b7fb88a9723cf5b76d456eb0b738' => array(
-		'version' => '6.13.8.0-alpha1750695105',
+		'version' => '6.13.8.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/actions.php'
 	),
 	'd4eb94df91a729802d18373ee8cdc79f' => array(
-		'version' => '4.2.6.0-alpha1750347518',
+		'version' => '4.2.6.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-backup/actions.php'
 	),
 );
