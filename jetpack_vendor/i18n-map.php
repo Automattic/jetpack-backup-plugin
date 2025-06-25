202cb22e4f9eed62332ce985b12273d05db50262
tbradsha <tbradsha@users.noreply.github.com>
2025-06-25 16:14:34 +0000

    Phan: Clean up `PhanRedundantCondition` violations (excluding jetpack and crm) (#44033)
    * packages/connection: PhanRedundantCondition
    
    * packages/device-detection: PhanRedundantCondition
    
    * packages/forms: PhanRedundantCondition
    
    * packages/jetpack-mu-wpcom: PhanRedundantCondition
    
    * packages/my-jetpack: PhanRedundantCondition
    
    * packages/search: PhanRedundantCondition
    
    * packages/status: PhanRedundantCondition
    
    * packages/sync: PhanRedundantCondition
    
    * packages/videopress: PhanRedundantCondition
    
    * packages/wp-js-data-sync: PhanRedundantCondition
    
    * plugins/boost: PhanRedundantCondition
    
    * plugins/super-cache: PhanRedundantCondition
    
    * plugins/vaultpress: PhanRedundantCondition
    
    * plugins/wpcomsh: PhanRedundantCondition
    
    * Add changelog
    
    * Trade a few Phan violations back for some compliant PHPCS
    
    * Update Phan baselines
    
    * Remove errant code
    
    * Update PHPDoc
    
    * Update projects/plugins/super-cache/wp-cache-phase2.php
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    * Explicitly initialize $cached_direct_pages early
    
    * Ensure we have what we need
    
    * Make Phan happier
    
    * Update condition to use int instead of bool
    
    * Update Phan baseline
    
    ---------
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15881245606
    
    Upstream-Ref: Automattic/jetpack@7f361d98b695f1ae6d8cf1ff830ce9c14abcb863

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -30,7 +30,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.13.9',
+      'ver' => '6.13.10-alpha1750867098',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.16.8-alpha1750780760',
+      'ver' => '5.16.8-alpha1750867098',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -70,7 +70,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.14.1',
+      'ver' => '4.14.2-alpha1750867098',
     ),
   ),
 );
