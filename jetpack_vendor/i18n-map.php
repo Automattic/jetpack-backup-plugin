785df3a7be61b9f258fd34406eed5acb2154ad20
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2025-09-05 06:09:25 +0000

    Update wordpress monorepo (#45027)
    * Update wordpress monorepo
    
    * Remove unwarranted change
    
    * Remove framer-motion hack
    
    Officially bumped here:
    https://github.com/WordPress/gutenberg/pull/71442
    
    * Update pnpm-lock.yaml
    
    * Add changelogs
    
    * Use moreHorizontal instead of moreHorizontalMobile
    
    https://github.com/WordPress/gutenberg/pull/71172
    
    * Fix some typecheck errors
    
    * Remove ESLint comment
    
    * Fix TypeScript parse option errors in useSimpleQuery and useSimpleMutation
    
    * Use a better fix for parse option
    
    * Fix Instagram Gallery tests: mock @wordpress/api-fetch instead of window.fetch
    
    * Fix extension block tests: mock @wordpress/api-fetch instead of window.fetch
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    Co-authored-by: Grzegorz Chudzinski-Pawlowski <112354940+grzegorz-cp@users.noreply.github.com>
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17484937851
    
    Upstream-Ref: Automattic/jetpack@28199bfc8a894504b21f736b9e18426210ec8f8b

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.5',
+      'ver' => '4.3.6-alpha1757051536',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '4.2.19',
+      'ver' => '4.2.20-alpha1757051536',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.18.6-alpha1756991872',
+      'ver' => '6.18.6-alpha1757051536',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.3.8',
+      'ver' => '0.3.9-alpha1757051536',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.3',
+      'ver' => '4.3.4-alpha1757051536',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.26.0-alpha1757014721',
+      'ver' => '5.26.0-alpha1757051536',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
