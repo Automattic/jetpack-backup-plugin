181bba2f2c8f03522edd60f419d80d1a7364a5ed
tbradsha <tbradsha@users.noreply.github.com>
2025-09-08 22:40:46 +0000

    Updated package versions for automattic/jetpack-backup-plugin
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17566051236
    
    Upstream-Ref: Automattic/jetpack@d00e5cf1b6a709547f35b02a5fe98e737608e8ad

--- vendor/jetpack-autoloader/class-plugin-locator.php
+++ vendor/jetpack-autoloader/class-plugin-locator.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp9559eef123208b7d1b9c15b978567267_backupⓥ3_5_alpha\al5_0_9;
+namespace Automattic\Jetpack\Autoloader\jp9559eef123208b7d1b9c15b978567267_backupⓥ3_5\al5_0_9;
 
  // phpcs:ignore
 
