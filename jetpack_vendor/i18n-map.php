00b333aa616fb6c24797e7ca3fc59e24e999b21d
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2025-09-22 09:14:58 +0000

    Backport jetpack 15.1-a.7 Changes (#45266)
    * Changelog and readme.txt edits.
    
    * Apply suggestions from code review
    
    Co-authored-by: Gergely Márk Juhász <36671565+gmjuhasz@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Gergely Márk Juhász <36671565+gmjuhasz@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17910095080
    
    Upstream-Ref: Automattic/jetpack@6c109984cd6a7e9daff77e9171a891ebc72b9b98

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.8',
+      'ver' => '4.3.9',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '4.2.22',
+      'ver' => '4.2.23',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.18.9',
+      'ver' => '6.18.10',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.3.10',
+      'ver' => '0.3.11',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.5',
+      'ver' => '4.3.6',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.27.0',
+      'ver' => '5.27.1',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
