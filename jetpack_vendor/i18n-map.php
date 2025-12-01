88ec206657b790b7275d1a9015b0e42b53a22f19
anomiex <anomiex@users.noreply.github.com>
2025-12-01 18:18:24 +0000

    Update wordpress monorepo (#46143)
    * Update wordpress monorepo
    * publicize-components: Remove no-longer-needed `@ts-expect-error`
    * my-jetpack: Account for change of timezone offset from string to number
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/19832478827
    
    Upstream-Ref: Automattic/jetpack@398ccf2c946e650f2fdf84a032ed137c6aa14fdc

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.13',
+      'ver' => '4.3.14-alpha1764612022',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '4.2.31',
+      'ver' => '4.2.32-alpha1764612022',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.19.9-alpha1764234497',
+      'ver' => '6.19.9-alpha1764612022',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.0',
+      'ver' => '0.4.1-alpha1764612022',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.9',
+      'ver' => '4.3.10-alpha1764612022',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.28.8-alpha1764193548',
+      'ver' => '5.28.8-alpha1764612022',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
