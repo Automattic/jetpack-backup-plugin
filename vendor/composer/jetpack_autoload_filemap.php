ddcdafa22078cb18752627852369b1393515a3da
anomiex <anomiex@users.noreply.github.com>
2026-01-06 18:07:37 +0000

    Update wordpress monorepo (#46456)
    * Update wordpress monorepo.
    * Downgrade `@wordpress/build` 0.x deps.
    * pnpmfile hack for `@wordpress/image-cropper`.
    * publicize-components: Add some `@ts-expect-error`.
    * publicize-components: Set `TZ=UTC` for tests so a date-related test
      doesn't fail on systems west of UTC.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20756940780
    
    Upstream-Ref: Automattic/jetpack@27f88f0d404a438afe000ff5c51384ffb6b97227

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,15 +7,15 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '4.3.18.0',
+		'version' => '4.3.19.0-alpha1767721690',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'7372b7fb88a9723cf5b76d456eb0b738' => array(
-		'version' => '6.20.0.0-alpha1767709040',
+		'version' => '6.20.0.0-alpha1767721690',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/actions.php'
 	),
 	'd4eb94df91a729802d18373ee8cdc79f' => array(
-		'version' => '4.2.35.0',
+		'version' => '4.2.36.0-alpha1767721690',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-backup/actions.php'
 	),
 );
