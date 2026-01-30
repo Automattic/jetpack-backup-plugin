6005c9f465f5aa386bb293fa1d04e27a93e79d52
anomiex <anomiex@users.noreply.github.com>
2026-01-30 18:09:31 +0000

    Update wordpress monorepo (#46854)
    * Update wordpress monorepo
    * Charts: Fix TypeScript error in story by using spacing instead of gap on HStack.
    * Components: Fix TypeScript errors with WordPress components type changes
    * Cast modal title to string in ThreatModal
    * Add label to threats status toggle group
    * Remove unused component
    * Fix type errors related to ExternalLink missing children
    * Fix icon type in forms actions
    * Fix the remaining TS errors in forms
    * NavigableMenu passes stopNavigationEvents by default
    * Use controlled value for Filename TextControl
    * Fix errors in my jetpack
    * Fix publicize errors except tab panel one to avoid conflicts with #46856
    * Fix multiple errors in jetpack blocks
    * Suppress errors in the remaining publicize components for now
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/21525260613
    
    Upstream-Ref: Automattic/jetpack@ab92a74db764763e0805d1a2995a215d3399040c

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,11 +10,11 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.22-alpha1769720062',
+      'ver' => '4.3.22-alpha1769795377',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '4.2.38',
+      'ver' => '4.2.39-alpha1769795377',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '7.0.0-alpha1769696240',
+      'ver' => '7.0.0-alpha1769795377',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.8',
+      'ver' => '0.4.9-alpha1769795377',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.17',
+      'ver' => '4.3.18-alpha1769795377',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.30.4-alpha1769652235',
+      'ver' => '5.30.4-alpha1769795377',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
