536ade3eac5aaf123a7b1b8d4f7665aa50721df5
gmjuhasz <gmjuhasz@users.noreply.github.com>
2026-02-18 17:39:14 +0000

    Backport social 8.0.0 changes (#47191)
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    * Remove prefix in Social changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/22150087149
    
    Upstream-Ref: Automattic/jetpack@d6b48a9eedc31e42367423cf270c23175622dfd8

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.25-alpha1771364022',
+      'ver' => '4.3.25',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '7.1.1-alpha1771364022',
+      'ver' => '7.1.1',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.12-alpha1771364022',
+      'ver' => '0.4.12',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.22-alpha1771364022',
+      'ver' => '4.3.22',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.31.4-alpha1771425941',
+      'ver' => '5.31.5-alpha1771435203',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
