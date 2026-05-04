60c7354c520596bd6834d9bcbc9ebbb55879353a
tbradsha <tbradsha@users.noreply.github.com>
2026-05-04 18:21:01 +0000

    Backport jetpack 15.8-beta changes (#48493)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/25334954093
    
    Upstream-Ref: Automattic/jetpack@4344d2acd33ba0f5440c8df310d8a39b6e6d1235

--- vendor/jetpack-autoloader/class-shutdown-handler.php
+++ vendor/jetpack-autoloader/class-shutdown-handler.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp9559eef123208b7d1b9c15b978567267_backupⓥ3_9_alpha\al5_0_17_alpha;
+namespace Automattic\Jetpack\Autoloader\jp9559eef123208b7d1b9c15b978567267_backupⓥ3_9_alpha\al5_0_17;
 
  // phpcs:ignore
 
