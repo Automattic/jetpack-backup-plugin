ed4e933dbc3ff3b60a5f1e724c65395e0f57137e
anomiex <anomiex@users.noreply.github.com>
2026-05-14 18:34:47 +0000

    Clean up boilerplate `.gitattributes` (#48802)
    Add a lot of generic tooling `production-excludes` entries to the
    monorepo root `.gitattributes`, and remove them from project
    `.gitattributes`.
    
    Also, have the mirror build always generate a `.gitattributes` that sets
    `export-ignore` for `.git*` files, and additionally strip the setting of
    any `production-include` and `production-exclude` attributes in the
    mirror copy.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/25877340776
    
    Upstream-Ref: Automattic/jetpack@bf1983deeacdfdea13fdfb490eae62222967b6fc

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,15 +7,15 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '4.3.36.0',
+		'version' => '4.3.37.0-alpha1778782620',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'7372b7fb88a9723cf5b76d456eb0b738' => array(
-		'version' => '8.3.5.0',
+		'version' => '8.3.6.0-alpha1778782620',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/actions.php'
 	),
 	'd4eb94df91a729802d18373ee8cdc79f' => array(
-		'version' => '4.2.56.0-alpha1778699413',
+		'version' => '4.2.56.0-alpha1778782620',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-backup/actions.php'
 	),
 );
