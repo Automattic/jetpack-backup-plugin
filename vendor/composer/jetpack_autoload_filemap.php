4effd3a163144e7ae9dc7707d8f4eedfb9ce0299
tbradsha <tbradsha@users.noreply.github.com>
2026-06-02 20:10:54 +0000

    Tests: Split code coverage scripts into test-js-coverage and test-php-coverage (#48845)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/26844277266
    
    Upstream-Ref: Automattic/jetpack@b25e59181090ffcd52db1200e882dbb26ebc543e

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,15 +7,15 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '4.3.39.0',
+		'version' => '4.3.40.0-alpha1780429966',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'7372b7fb88a9723cf5b76d456eb0b738' => array(
-		'version' => '8.5.2.0',
+		'version' => '8.5.3.0-alpha1780429966',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/actions.php'
 	),
 	'd4eb94df91a729802d18373ee8cdc79f' => array(
-		'version' => '4.3.2.0-alpha1780428861',
+		'version' => '4.3.2.0-alpha1780429966',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-backup/actions.php'
 	),
 );
