bfa19b417c3ced7e458f037f82062233c0688a50
zaguiini <zaguiini@users.noreply.github.com>
2026-06-02 19:50:56 +0000

    Agents Manager: Release 0.1.0 (#49354)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/26843319915
    
    Upstream-Ref: Automattic/jetpack@427fff29bfe61a83e72a52df9f4657794b97ed72

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,15 +6,15 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.8.7',
+      'ver' => '0.8.8',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.38',
+      'ver' => '4.3.39',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '4.3.1',
+      'ver' => '4.3.2-alpha1780428861',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -30,11 +30,11 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.5.1',
+      'ver' => '8.5.2',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.4.26',
+      'ver' => '0.4.27-alpha1780428861',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -42,7 +42,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.3.37',
+      'ver' => '4.3.38-alpha1780428861',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -78,7 +78,7 @@ return array(
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
-      'ver' => '0.1.11',
+      'ver' => '0.1.12-alpha1780428861',
     ),
   ),
 );
