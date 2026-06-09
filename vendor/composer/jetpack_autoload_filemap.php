7fef09ce8e15922983e9512182816c2aa8221867
anomiex <anomiex@users.noreply.github.com>
2026-06-09 18:39:49 +0000

    Update dependency concurrently to v10 (#49492)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27226904998
    
    Upstream-Ref: Automattic/jetpack@1919330964d0118b85dfc70942b9e8073e6df076

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,7 +7,7 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '4.3.42.0-alpha1780949022',
+		'version' => '4.3.42.0-alpha1781029404',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'7372b7fb88a9723cf5b76d456eb0b738' => array(
@@ -15,7 +15,7 @@ return array(
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/actions.php'
 	),
 	'd4eb94df91a729802d18373ee8cdc79f' => array(
-		'version' => '4.3.3.0-alpha1781005495',
+		'version' => '4.3.3.0-alpha1781029404',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-backup/actions.php'
 	),
 );
