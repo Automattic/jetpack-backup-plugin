da0d815020707177b52fac5ff654d4d1a989d771
kangzj <kangzj@users.noreply.github.com>
2026-07-22 04:12:45 +0000

    Backport jetpack 16.1-a.3 changes (#50674)
    * Baseline changelogger run
    
    * Changelog refinement
    
    * Changelog sort
    
    * Shorten verbose changelog entries
    
    * Changelog and readme.txt edits.
    
    * Update stable tag in readme.txt
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29889522001
    
    Upstream-Ref: Automattic/jetpack@285ea6d30bb2dbb0377e8fd8f3775956245b1617

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,15 +7,15 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '4.4.6.0-alpha1784214611',
+		'version' => '4.4.6.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'7372b7fb88a9723cf5b76d456eb0b738' => array(
-		'version' => '8.7.9.0-alpha1784298177',
+		'version' => '8.7.9.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/actions.php'
 	),
 	'd4eb94df91a729802d18373ee8cdc79f' => array(
-		'version' => '4.3.9.0-alpha1784308796',
+		'version' => '4.3.9.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-backup/actions.php'
 	),
 );
