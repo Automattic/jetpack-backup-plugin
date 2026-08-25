abd302e2215291d2a3a48305b558f7ae636575ad
chrisbliss18 <chrisbliss18@users.noreply.github.com>
2026-08-25 13:24:25 +0000

    Backport jetpack 16.2-a.3 changes (#51545)
    * Changelog and readme.txt edits.
    
    * Sort 16.2-a.3 changelog entries
    
    * Update stable tag in readme.txt
    
    ---------
    
    Co-authored-by: Chris Jean <chris.jean@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32851203157
    
    Upstream-Ref: Automattic/jetpack@4a44540817f65c19c444f4a67315cc132632aca3

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -11,11 +11,11 @@ return array(
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'7372b7fb88a9723cf5b76d456eb0b738' => array(
-		'version' => '8.12.0.0-alpha1787647592',
+		'version' => '8.12.0.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/actions.php'
 	),
 	'd4eb94df91a729802d18373ee8cdc79f' => array(
-		'version' => '4.3.15.0-alpha1787574999',
+		'version' => '4.3.15.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-backup/actions.php'
 	),
 );
