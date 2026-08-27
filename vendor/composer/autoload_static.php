f4dab1a9e369b5ad4bf6a458294cbc9eb73e0138
retrofox <retrofox@users.noreply.github.com>
2026-08-27 07:54:24 +0000

    WP Build Polyfills: continue the admin menu color behind the boot frame (#51619)
    * fix boot frame backdrop on unknown admin schemes
    
    sample the wp-admin menu color behind the boot single-page layout
    
    * add plugin changelogs for boot frame backdrop
    
    * match the boot layout by its CSS Modules class
    
    boot 0.21 (gutenberg#81756) renames boot-layout--single-page
    
    * suppress phan duplicate statement in frame test
    
    the second register() call is the idempotence assertion
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33050346657
    
    Upstream-Ref: Automattic/jetpack@18bdbaf671096ed9bcceb2e01ac70ee1484317fc

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -266,6 +266,7 @@ class ComposerStaticInit9559eef123208b7d1b9c15b978567267_backupⓥ4_0_alpha
         'Automattic\\Jetpack\\Terms_Of_Service' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-connection/src/class-terms-of-service.php',
         'Automattic\\Jetpack\\Tracking' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-connection/src/class-tracking.php',
         'Automattic\\Jetpack\\WP_Abilities\\Registrar' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-wp-abilities/src/class-registrar.php',
+        'Automattic\\Jetpack\\WP_Build_Polyfills\\WP_Build_Admin_Frame' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-wp-build-polyfills/src/class-wp-build-admin-frame.php',
         'Automattic\\Jetpack\\WP_Build_Polyfills\\WP_Build_Polyfills' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-wp-build-polyfills/src/class-wp-build-polyfills.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'Jetpack_IXR_Client' => __DIR__ . '/../..' . '/jetpack_vendor/automattic/jetpack-connection/legacy/class-jetpack-ixr-client.php',
