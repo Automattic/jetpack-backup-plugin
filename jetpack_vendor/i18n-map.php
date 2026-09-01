0b233da29589b1c3ca401fbcff304950cb52ad42
chrisbliss18 <chrisbliss18@users.noreply.github.com>
2026-09-01 17:41:04 +0000

    Backport jetpack 16.2-a.5 changes (#51843)
    * Changelog and readme.txt edits.
    
    * Include Jetpack AI settings fix in 16.2-a.5
    
    * Update stable tag in readme.txt
    
    ---------
    
    Co-authored-by: Chris Jean <chris.jean@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33536961377
    
    Upstream-Ref: Automattic/jetpack@257852d4b1145bd2022ae7590b22dc22823ccc4e

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,19 +6,19 @@ return array(
   'packages' => array(
     'jetpack-activity-log' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-activity-log',
-      'ver' => '0.3.1-alpha1788269986',
+      'ver' => '0.3.1',
     ),
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.11.1-alpha1787829579',
+      'ver' => '0.11.1',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '5.0.0',
+      'ver' => '5.0.1',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '5.0.0-alpha1788269986',
+      'ver' => '5.0.0',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
@@ -34,7 +34,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '9.1.0-alpha1788269986',
+      'ver' => '9.1.0',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '5.0.1-alpha1788190106',
+      'ver' => '5.0.1',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
@@ -58,7 +58,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '6.1.0-alpha1788269986',
+      'ver' => '6.1.0',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -78,7 +78,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '5.0.1-alpha1787942239',
+      'ver' => '5.0.1',
     ),
     'jetpack-wp-abilities' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-abilities',
@@ -86,7 +86,7 @@ return array(
     ),
     'jetpack-wp-build-polyfills' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wp-build-polyfills',
-      'ver' => '0.5.1-alpha1788269986',
+      'ver' => '0.5.1',
     ),
   ),
   'paths' => array(
