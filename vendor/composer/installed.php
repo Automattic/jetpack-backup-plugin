5d9a40e007c1d8a39ffd48ad70dc9d15d3420a34
kat3samsin <kat3samsin@users.noreply.github.com>
2026-09-03 14:30:28 +0000

    Jetpack AI: launch feature controls on self-hosted sites (#51909)
    * Jetpack AI: enforce self-hosted controls
    
    * Jetpack AI: show self-hosted feature controls
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33765489463
    
    Upstream-Ref: Automattic/jetpack@2e6b53b6e9fae740282fbea3092323513adb27bf

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -58,7 +58,7 @@
         'automattic/jetpack-backup' => array(
             'pretty_version' => '5.0.1-alpha.1788442733',
             'version' => '5.0.1.0-alpha1788442733',
-            'reference' => '986103bda513b8e0f2b78393ed899c2ef91c8263',
+            'reference' => 'f0b7cb5c26d9ae55dd6c23b9ca7e2c4d6550b751',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-backup',
             'aliases' => array(),
@@ -200,9 +200,9 @@
             'dev_requirement' => false,
         ),
         'automattic/jetpack-my-jetpack' => array(
-            'pretty_version' => '6.1.1-alpha.1788342643',
-            'version' => '6.1.1.0-alpha1788342643',
-            'reference' => '1b989b544f22774959dbe6cc9972cd1e444f41d7',
+            'pretty_version' => '6.2.0-alpha.1788444744',
+            'version' => '6.2.0.0-alpha1788444744',
+            'reference' => '730620d727981b6937faf4aba42b3908787dd135',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-my-jetpack',
             'aliases' => array(),
