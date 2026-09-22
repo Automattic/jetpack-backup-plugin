5d53083cbbea67a8089e133d86567bd541f03442
anomiex <anomiex@users.noreply.github.com>
2026-09-22 18:52:59 +0000

    Jetpack: Exclude various JS/TS source files from published plugin (#52650)
    Jetpack plugin:
    
    * Bunch of stuff in `_inc/shared/`
    * `_inc/site-switcher.jsx`
    * `modules/subscriptions/email-design-editor/src/`
    
    Packages:
    
    * Agents manager: `src/js/`
    * Publicize: `build/assets/index.d.ts`
    * VideoPress: `wordpress-augmentations.d.ts`
    * Wp-build polyfills: `src/js/`
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35767992962
    
    Upstream-Ref: Automattic/jetpack@1e90e98d09b65e85ce786b022406dd6ec1cc1f8e

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -22,7 +22,7 @@
         'automattic/jetpack-activity-log' => array(
             'pretty_version' => '0.4.1',
             'version' => '0.4.1.0',
-            'reference' => '2c2c0da9b9377e5918cbb58f511921f5699bafe4',
+            'reference' => '5869946c40d0c9007bf0d477b91c6b84bd6c53ff',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-activity-log',
             'aliases' => array(),
@@ -58,7 +58,7 @@
         'automattic/jetpack-backup' => array(
             'pretty_version' => '5.0.5-alpha.1790095377',
             'version' => '5.0.5.0-alpha1790095377',
-            'reference' => '8e7878f8c56a805af60aff473baaa1a5e7249ed3',
+            'reference' => '03241a96a01d90c8713f8f63afbeda0e4da4b5cc',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-backup',
             'aliases' => array(),
@@ -211,7 +211,7 @@
         'automattic/jetpack-my-jetpack' => array(
             'pretty_version' => '6.5.0-alpha.1790095377',
             'version' => '6.5.0.0-alpha1790095377',
-            'reference' => 'a0a7c514b12e41a8126497b9e53be93aef087f44',
+            'reference' => 'c57d77c8c1577ceb6f950599477fd7a27ab9e675',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-my-jetpack',
             'aliases' => array(),
@@ -308,9 +308,9 @@
             'dev_requirement' => false,
         ),
         'automattic/jetpack-wp-build-polyfills' => array(
-            'pretty_version' => '0.5.7',
-            'version' => '0.5.7.0',
-            'reference' => 'f2310268ac30595b2ed759d079bcc5a1c56833f7',
+            'pretty_version' => '0.5.8-alpha.1790102028',
+            'version' => '0.5.8.0-alpha1790102028',
+            'reference' => 'acae79b09d1877f827743b73d744445559beeb78',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-wp-build-polyfills',
             'aliases' => array(),
